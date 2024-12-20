a8432aef8013ea00e3179f4e4fc9123404cbd346
German pc <salazar.germandiego@gmail.com>
2024-12-19 23:01:41 -0300

    instalacion de react router6.4 y pantalla de inicio

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import { Tarjeta } from './components/tarjeta';
 function App() {
     return (
         <div className="flex">
-            <div>
+            <div className="h-screen">
                 <Sidebarra />
             </div>
-            <div className="pt-2">
+            <div className="w-full p-40 grid grid-cols-2 gap-6 justify-center items-center">
                 <Tarjeta />
                 <Tarjeta />
                 <Tarjeta />
