cda387b8a8a51b52c0dfda345e83c6ad0ac49148
German Salazar <salazar.germandiego@gmail.com>
2024-12-27 16:47:20 -0300

    creacion de carpetas para routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import { Button } from 'flowbite-react';
+import { Sidebarra } from './components/sidebarra';
+import { Tarjeta } from './components/tarjeta';
 
 function App() {
     return (
-        <>
-            <div>
-                <Button>Click me</Button>
+        <div className="flex">
+            <div className="h-screen">
+                <Sidebarra />
             </div>
-        </>
+            <div className="grid grid-cols-2 gap-4 p-52 justify-items-center">
+                <Tarjeta />
+                <Tarjeta />
+                <Tarjeta />
+                <Tarjeta />
+            </div>
+        </div>
     );
 }
 
